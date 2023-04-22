ec2bdfd8a2d7427d30a4698e8c60d167de6288cf
Michael Morales <121997408+MichaelMorales64@users.noreply.github.com>
2023-04-22 12:05:53 -0700

    Add files via upload

---
+++ NetflixProject.sql
@@ -0,0 +1,197 @@
+/* In this SQL project, I analyze my own Netflix data to gain insights into my viewing habits,
+such as which shows or movies I watch the most, what genres I prefer, and when I tend to watch Netflix the most.
+I also explore patterns in my viewing behavior, such as binge-watching habits and the average length of my viewing sessions. */
+
+--Skills used: CASE, CTE's, WINDOW FUNCTIONS, AGGREGATE FUNCTIONS, CONVERTING DATA TYPES,
+
+-- Getting an overall view of my viewing history
+SELECT * FROM netflixviewinghistory
+
+
+-- Checking data types
+SELECT column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'netflixviewinghistory'
+
+
+--Updating the table with additional columns for season and episode name
+ALTER TABLE netflixviewinghistory
+ADD "Season" VARCHAR(255);
+
+ALTER TABLE netflixviewinghistory
+ADD "EpisodeName"  VARCHAR(255);
+
+
+
+-- Splitting title into separate columns for season and episode name
+
+-- extracting everything before the first colon without including the colon itself, in the title column/string 
+UPDATE netflixviewinghistory
+SET Title =
+    CASE 
+        WHEN CHARINDEX(':', title) > 0 then SUBSTRING(title, 1, CHARINDEX(':', title) - 1) 
+        ELSE title 
+    END, 
+--
+Season = 
+    CASE 
+        WHEN CHARINDEX(':', title, CHARINDEX(':', title) + 1) > 0 
+            THEN SUBSTRING(title, CHARINDEX(':', title) + 1, CHARINDEX(':', title, CHARINDEX(':', title) + 1) - CHARINDEX(':', title) - 1) 
+        ELSE '' 
+    END, 
+ EpisodeName =   
+    CASE 
+        WHEN CHARINDEX(':', title, CHARINDEX(':', title) + 1) > 0 
+            THEN SUBSTRING(title, CHARINDEX(':', title, CHARINDEX(':', title) + 1) + 1, LEN(title)) 
+        ELSE '' 
+    END;
+
+
+-- Removing whitspaces from columns
+UPDATE netflixviewinghistory
+SET Title = TRIM(Title),
+    Season = TRIM(Season),
+	EpisodeName = TRIM(EpisodeName);
+
+
+
+-- Updating values in episode name column to remove foreign characters
+SELECT DISTINCT EpisodeName FROM netflixviewinghistory;
+
+UPDATE netflixviewinghistory
+SET EpisodeName = CASE WHEN EpisodeName = 'IT’S GOOD TO BE BACK ON THE MOON' THEN 'IT''S GOOD TO BE BACK ON THE MOON'
+                       WHEN EpisodeName = 'Piña Colada' THEN 'Pina Colada'
+					   ELSE EpisodeName
+					   END ;
+
+
+-- Categorizing into show or movie
+ALTER TABLE netflixviewinghistory
+ADD "Category"  VARCHAR(255);
+
+UPDATE netflixviewinghistory
+SET
+Category = 
+    CASE 
+	    WHEN Season like '%Season%' THEN 'Show'
+	    WHEN Season like '%Part%' THEN 'Show'
+        WHEN EpisodeName like '%Epsiode%' THEN 'Show'
+	    WHEN Title in ('That ''90s Show',
+	                'Living with Yourself',
+					'exception',
+					'PIECES OF HER',
+					'Away',
+					'1899',
+					'I AM A STALKER',
+					'Oats Studios') THEN 'Show'
+	ELSE 'Movie'
+		END;
+
+
+-- Updating shows with null values in Season column to reflect 'Season 1'
+UPDATE netflixviewinghistory
+SET Season = 
+       CASE 
+	       WHEN Season = '' and Category = 'Show' THEN 'Season 1'
+       ELSE Season
+          END;
+
+
+-- Updating the genre for all shows/movies
+ALTER TABLE netflixviewinghistory
+ADD "Genre"  VARCHAR(255);
+
+UPDATE netflixviewinghistory
+SET	Genre =
+CASE WHEN Title = 'You' THEN 'Psychological Thriller'
+     WHEN Title = '1899' THEN 'Mystery'
+	 WHEN Title in ('21 Jump Street','Friends from College','Glass Onion','Living with Yourself','Space Force','That ''90s Show','The Recruit','This Is the End','Wednesday','You People') THEN 'Comedy'
+	 WHEN Title in ('Another Life','Awake','Away','Code 8','Don''t Look Up','Elysium','Exception','I Am Mother','IO','JUNG_E','Oats Studios','Outside the Wire','Oxygen','Stowaway','The 100',
+'The Cloverfield Paradox','The Colony','The Midnight Sky','The Mist','The OA','Travelers') THEN 'Sci-fi'
+     WHEN Title in ('Bird Box','Bullet Head','PIECES OF HER','Prisoners','Shimmer Lake','Spiderhead') THEN 'Thriller'
+	 WHEN Title in ('The Hurt Locker','Black Crab','War') THEN 'War'
+	 WHEN Title = 'Blow' THEN 'Crime'
+	 WHEN Title in ('Bullet Train','Extraction','How It Ends','Mile 22','No Escape','Resident Evil','Southpaw','Spectral','TAU','The Gray Man','The Guilty','Triple Frontier') THEN 'Action'
+	 WHEN Title in ('Call Me by Your Name','Designated Survivor','Ozark','The King') THEN 'Drama'
+     WHEN Title = 'Day Shift' THEN 'Supernatual'
+     WHEN Title in ('I AM A STALKER','Our Universe','Stutz','The Hatchet Wielding Hitchhiker','The Volcano') THEN 'Documentary'
+     WHEN Title in ('Slumberland','The Sandman') THEN 'Fantasy'
+     WHEN Title = 'The Walking Dead' THEN 'Horror'
+END; 
+
+
+
+-- The date range of viewing history
+SELECT MIN(Date) as MinDate, MAX(Date) as MaxDate 
+FROM NetflixViewingHistory;
+
+
+-- Favorite (most watched) genre for shows and movies
+SELECT Genre,GenreCount FROM (
+  SELECT Genre, COUNT(Genre) AS GenreCount,ROW_NUMBER() OVER (ORDER BY COUNT(Genre) DESC) AS rn
+  FROM netflixviewinghistory
+  GROUP BY Genre
+)
+FavGenre
+WHERE rn = 1;
+
+
+-- Most watched tv show (Space Force)
+SELECT Title, ShowCount
+FROM (
+   SELECT Title, SUM(1) AS ShowCount, ROW_NUMBER() OVER (ORDER BY SUM(1) DESC) AS rn
+   FROM NetflixViewingHistory
+   WHERE Category = 'Show'
+   GROUP BY Title
+) MostWatched
+WHERE rn= 1;
+
+
+-- Least watched genre for shows and movies (Crime)
+WITH GenreLeast AS (
+  SELECT Genre, COUNT(genre) AS GenreTotal,ROW_NUMBER() OVER (ORDER BY COUNT(Genre)) AS rn
+  FROM netflixviewinghistory
+  GROUP BY Genre
+)
+SELECT Genre
+FROM genreleast
+WHERE rn = 1;
+
+
+-- Least watched tv shows (Designated Survivor, exception, Friends from College, That 90's Show, The Recruit, TWD)
+ SELECT Title 
+ FROM netflixviewinghistory
+ WHERE Category = 'Show'
+ GROUP BY Title
+ HAVING COUNT(Title) < 2;
+
+
+-- How many movies I've watched in 2023 (15)
+SELECT Category,COUNT(Title) AS MovieTotal
+FROM netflixviewinghistory
+WHERE Category = 'Movie'
+AND YEAR(Date) = '2023'
+GROUP BY Category;
+
+
+-- Average number of shows I watch a day (3)
+WITH ShowAVG AS (
+  SELECT Date AS Viewing_Date, COUNT(Title) as TitleTotal
+  FROM netflixviewinghistory
+  WHERE Category = 'Show'
+  GROUP BY Date
+  )
+SELECT AVG(TitleTotal) AS Average
+FROM ShowAVG;
+
+
+-- Average number of movies I watch a day (1)
+WITH MovieAVG AS (
+  SELECT Date AS Viewing_Date, COUNT(Title) as TitleTotal
+  FROM netflixviewinghistory
+  WHERE Category = 'Movie'
+  GROUP BY Date
+  )
+SELECT AVG(TitleTotal) AS Average
+FROM MovieAVG
+;
